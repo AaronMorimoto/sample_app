c6603d4218d902955b83ed9a2eb427f51694aac3
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2022-12-10 07:52:45 +0000

    sample_app7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -34,6 +34,6 @@ class ListsController < ApplicationController
   private
   # ストロングパラメータ--"マスアサインメント脆弱性"を防ぐ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
